1e63bb93fb3b565802661a8501ffc2bc4912b42b
Taleren <122693648+Taleren@users.noreply.github.com>
2024-10-15 19:03:18 +0200

    escenario again

--- Nos vemos Abajo/Assets/Scripts2/basicObject.cs
+++ Nos vemos Abajo/Assets/Scripts2/basicObject.cs
@@ -6,10 +6,12 @@ using UnityEngine.Events;
 using UnityEngine.UI;
 public class basicObject : MonoBehaviour,IObject
 {
-  [SerializeField]  private InteractEvent intEvent;
+  [SerializeField] private InteractEvent intEvent;
+  [SerializeField] private GameObject outline;
     public string Hoverkey;
     public bool showName = true;
     MaterialPropertyBlock materialPropertyBlock;
+
 
     public virtual void EnterHover()
     {
@@ -17,10 +19,11 @@ public class basicObject : MonoBehaviour,IObject
         {
             UIManager.Instance.showObjectName(Hoverkey);
         }
-        materialPropertyBlock = new MaterialPropertyBlock();
-        materialPropertyBlock.SetColor("_Color", Color.white);
-        materialPropertyBlock.SetFloat("_isOutlined", 1.0f);
-        this.GetComponent<MeshRenderer>().SetPropertyBlock(materialPropertyBlock, 1);
+        //materialPropertyBlock = new MaterialPropertyBlock();
+        //materialPropertyBlock.SetColor("_Color", Color.white);
+        //materialPropertyBlock.SetFloat("_isOutlined", 1.0f);
+        //this.GetComponent<MeshRenderer>().SetPropertyBlock(materialPropertyBlock, 1);
+        outline.SetActive(true);
 
     }
 
@@ -32,7 +35,10 @@ public class basicObject : MonoBehaviour,IObject
     public virtual void LeaveHover()
     {
         UIManager.Instance.hideObjectName();
+        /*
         materialPropertyBlock.SetFloat("_isOutlined", 0.0f);
         this.GetComponent<MeshRenderer>().SetPropertyBlock(materialPropertyBlock, 1);
+        */
+        outline.SetActive(false);
     }
 }
