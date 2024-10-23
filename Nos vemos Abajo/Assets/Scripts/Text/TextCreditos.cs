75ccfd82396cd85bbfc1cdcfde709c05403af21a
Andyy728 <andrealb0112@gmail.com>
2024-10-23 18:36:38 +0200

    Créditos

---
+++ Nos vemos Abajo/Assets/Scripts/Text/TextCreditos.cs
@@ -0,0 +1,18 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using TMPro;
+using UnityEditor;
+using UnityEngine;
+using UnityEngine.InputSystem;
+
+[RequireComponent(typeof(TMP_Text))]
+
+public class TextCreditos : MonoBehaviour
+{
+    
+    
+}
+
